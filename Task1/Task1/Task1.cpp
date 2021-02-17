fbffdfebdd297718be6446bd586350af51f38ae0
blessedoregon <54234626+blessedoregon@users.noreply.github.com>
2021-02-17 14:08:14 +0300

    Add files via upload

---
+++ Task1/Task1/Task1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void main()
+{
+    int x = 2; //Task1
+    double f;
+    f = 1 / (sqrt(x) + sqrt(2));
+    cout.precision(4);
+    cout << f;
+
+    cout << " \nvvedite x \n";
+    cin >> x;
+    f = 1 / (sqrt(x) + sqrt(2));
+    cout << f;
+}
